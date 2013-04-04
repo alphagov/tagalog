40943c7a13eb5c9d733db5279e81e9fc9d27749a
Dan Carley <dan.carley@gmail.com>
2013-04-04 11:10:54 +0100

    [#46836025] Remove --no-stamp logship integ test
    Remove the `--no-stamp` option from the logship integration test when
    passing in JSON. This makes little sense for logship, since we always want
    to ship with a timestamp. We also need to ensure that a TS in JSON is
    preserved.
    
    Split timestamp generation (when JSON doesn't contain TS) out to a seaparate
    test which is marked as skipped until we can figure a way to mock the call
    to `tagalog._now()`.

--- test/integration/test_logship.py
+++ test/integration/test_logship.py
@@ -1,11 +1,27 @@
 from ..helpers import assert_equal
+from nose.plugins.skip import SkipTest
 from subprocess import Popen, PIPE
 import json
 
-def test_json():
+
+def test_json_timestamp_generated():
+    # Skipped until we figure out a way to mock tagalog._now() as
+    # datetime.datetime(2013, 1, 1, 9, 0, 0, 0). I suspect it's not possible
+    # with the use of subprocess to spawn a separate interpreter.
+    raise SkipTest
+
     json_input = '{"@fields": {"handbags": "great", "why": "because"}}'
-    p = Popen('logship --no-stamp --json -s stdout',
+    p = Popen('logship --json -s stdout',
               shell=True, stdout=PIPE, stdin=PIPE)
     data_out, _ = p.communicate(input=json_input)
-    assert_equal({'@fields': { 'handbags': 'great', 'why': 'because'}},
+    assert_equal({'@timestamp': '2013-01-01T09:00:00.000000Z', '@fields': { 'handbags': 'great', 'why': 'because'}},
+                 json.loads(data_out.decode("utf-8")))
+
+
+def test_json_timestamp_included():
+    json_input = '{"@timestamp": "2013-01-01T09:00:00.000000Z", "@fields": {"handbags": "great", "why": "because"}}'
+    p = Popen('logship --json -s stdout',
+              shell=True, stdout=PIPE, stdin=PIPE)
+    data_out, _ = p.communicate(input=json_input)
+    assert_equal({'@timestamp': '2013-01-01T09:00:00.000000Z', '@fields': { 'handbags': 'great', 'why': 'because'}},
                  json.loads(data_out.decode("utf-8")))
