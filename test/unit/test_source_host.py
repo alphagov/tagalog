a01a5659eb7f270f1a108073fdd015af13b7a953
Dan Carley <dan.carley@gmail.com>
2013-04-10 17:07:23 +0100

    [#46898331] Add desired unit tests for source_host
    These reference the desired interface and behaviour.

--- test/unit/test_source_host.py
+++ test/unit/test_source_host.py
@@ -2,12 +2,48 @@ from ..helpers import assert_equal, patch
 import datetime
 from tagalog import source_host
 
+
 def test_no_source_host_defaults():
     data = [{'@message': 'one'},
             {'@message': 'two'},
             {'@message': 'three'}]
     out = source_host(data)
 
+    # FIXME: Replace 'default' with mocked method that looks up the FQDN of
+    # the machine which it is running on.
     assert_equal(next(out), {'@message': 'one', '@source_host': 'default'})
     assert_equal(next(out), {'@message': 'two', '@source_host': 'default'})
     assert_equal(next(out), {'@message': 'three', '@source_host': 'default'})
+
+
+def test_source_host_provided():
+    data = [{'@source_host': 'orangutan.zoo.tld', '@message': 'one'},
+            {'@source_host': 'orangutan.zoo.tld', '@message': 'two'},
+            {'@source_host': 'orangutan.zoo.tld', '@message': 'three'}]
+    out = source_host(data, source_host='chimpanzee.zoo.tld')
+
+    assert_equal(next(out), {'@source_host': 'orangutan.zoo.tld', '@message': 'one'})
+    assert_equal(next(out), {'@source_host': 'orangutan.zoo.tld', '@message': 'two'})
+    assert_equal(next(out), {'@source_host': 'orangutan.zoo.tld', '@message': 'three'})
+
+
+def test_source_host_provided_custom_key():
+    data = [{'@message': 'one'},
+            {'@message': 'two'},
+            {'@message': 'three'}]
+    out = source_host(data, source_host='orangutan.zoo.tld', key='host')
+
+    assert_equal(next(out), {'host': 'orangutan.zoo.tld', '@message': 'one'})
+    assert_equal(next(out), {'host': 'orangutan.zoo.tld', '@message': 'two'})
+    assert_equal(next(out), {'host': 'orangutan.zoo.tld', '@message': 'three'})
+
+
+def test_source_host_dont_overwrite_existing():
+    data = [{'@source_host': 'orangutan.zoo.tld', '@message': 'one'},
+            {'@source_host': 'orangutan.zoo.tld', '@message': 'two'},
+            {'@source_host': 'orangutan.zoo.tld', '@message': 'three'}]
+    out = source_host(data, source_host='chimpanzee.zoo.tld')
+
+    assert_equal(next(out), {'@source_host': 'orangutan.zoo.tld', '@message': 'one'})
+    assert_equal(next(out), {'@source_host': 'orangutan.zoo.tld', '@message': 'two'})
+    assert_equal(next(out), {'@source_host': 'orangutan.zoo.tld', '@message': 'three'})
