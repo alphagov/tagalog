e43eda27ade775b8d5d2c35afd2d40bf81130bd9
Garima/Phil <igarimasingh@gmail.com>
2013-05-07 11:59:06 +0100

    configuring statsd shipper to take custom metrics

--- tagalog/shipper/statsd.py
+++ tagalog/shipper/statsd.py
@@ -1,11 +1,18 @@
 from socket import socket, AF_INET, SOCK_DGRAM
+import re
 
 from tagalog.shipper.ishipper import IShipper
 
 class StatsdShipper(IShipper):
     def __init__(self,args,kwargs):
         self.sock = socket(AF_INET, SOCK_DGRAM)
+        self.metric = kwargs.get('metric', '%{@source_host}.%{@fields.status}')
 
     def ship(self, msg):
-        statsd_msg = msg['@source_host'] + "." + str(msg["@fields.status"]) + ":1|c"
-        self.sock.sendto(statsd_msg, ('127.0.0.1',8125))
+        self.sock.sendto(self.__statsd_msg(msg), ('127.0.0.1',8125))
+
+    def __statsd_msg(self, msg):
+        pattern = r'%{([^}]*)}'
+        replacement = lambda m: str(msg[m.group(1)])
+        realised_metric = re.sub(pattern, replacement, self.metric)
+        return realised_metric + ':1|c'
--- test/unit/shipper/test_statsd.py
+++ test/unit/shipper/test_statsd.py
@@ -6,7 +6,15 @@ from tagalog.shipper.statsd import StatsdShipper
 class TestStatsdShipper(object):
     @patch('tagalog.shipper.statsd.socket')
     def test_ship(self, socket_mock):
-        ss = StatsdShipper(None,None)
+        ss = StatsdShipper(None,{})
         ss.ship({"@fields.status": 304,"@source_host":"wilmaaaaa"})
 
         socket_mock.return_value.sendto.assert_called_with('wilmaaaaa.304:1|c',('127.0.0.1',8125))
+
+    @patch('tagalog.shipper.statsd.socket')
+    def test_ship_with_provided_metric(self, socket_mock):
+        kwargs = {'metric': '%{@source_host}.%{@fields.counter}'}
+        ss = StatsdShipper(None, kwargs)
+        ss.ship({"@fields.counter": 'test',"@source_host":"wilmaaaaa", "@fields.randomness":"ignored"})
+
+        socket_mock.return_value.sendto.assert_called_with('wilmaaaaa.test:1|c',('127.0.0.1',8125))
